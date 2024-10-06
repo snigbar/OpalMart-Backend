6a2e660b7e902f8c409a791d698d9a0f7d6b904d
snigbar <snigbar@outlook.com>
2024-10-06 17:48:36 +0600

    initialized with eslint prettier and husky

---
+++ src/app/index.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import Express from 'express';
+
+const app = Express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
